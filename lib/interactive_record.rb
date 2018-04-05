9a7cd5e3d6c8cf68b5fa61cccdf3061bb5771699
Denise Pendleton <dpendl1@hotmail.com>
2018-04-05 02:07:30 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,8 +56,8 @@ def table_name_for_insert
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(id)
-    sql = "SELECT * FROM #{self.table_name} WHERE id = '#{:id}'"
+  def self.find_by(attribute:value)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute:} = '#{value}'"
     DB[:conn].execute(sql)
 
   end
